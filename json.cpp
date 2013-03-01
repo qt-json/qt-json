e162a7d5d06276ae740388a9914943eb4798f519
Maxim V. Potapov <mpotapov@unison.com>
2013-03-01 14:34:35 +0400

    Qt5 compatibility

--- json.cpp
+++ json.cpp
@@ -594,7 +594,7 @@ static int nextToken(const QString &json, int &index)
 
         QChar c = json[index];
         index++;
-        switch(c.toAscii())
+        switch(c.toLatin1())
         {
                 case '{': return JsonTokenCurlyOpen;
                 case '}': return JsonTokenCurlyClose;
